32a2f7b63a33857bb90a51d8915df09f978cdad0
Zina-Charikh <zineb.charikh@gmail.com>
2023-10-23 13:50:47 +0200

    modif premier script

--- premierscript.sh
+++ premierscript.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo "Hello World"
-
-resultat=$(cat *2016*.ann | grep Location | cut -f 3 | sort | uniq -c | sort)
-echo -e "$resultat"
-
-
+#écrire un script qui donne le nombre de Location par année
+resultat2016=$(cat ./2016/*/* | grep -c Location)
+resultat2017=$(cat ./2017/*/* | grep -c Location)
+resultat2018=$(cat ./2018/*/* | grep -c Location)
+echo -e "le nombre de locations pour l'années 2016 : $resultat2016"
+echo -e "le nombre de locations pour l'années 2017 : $resultat2017"
+echo -e "le nombre de locations pour l'années 2018 : $resultat2018"
