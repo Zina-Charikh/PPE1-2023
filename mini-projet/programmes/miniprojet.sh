7c25b1ee4c1a18bcddf068bcf1cab4a352a58c60
Zina-Charikh <zineb.charikh@gmail.com>
2023-11-06 15:46:15 +0100

    ajout mini-projet

---
+++ mini-projet/programmes/miniprojet.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+fichier=$1
+number=1
+
+if [ $# -ne 1 ]
+then
+	echo "Aucun fichier saisi, veuillez saisir un chemin";
+	exit;
+fi
+
+while read -r line;
+do
+	echo -e "$number :\t"${line}
+	number=$(expr $number + 1)
+done < "$fichier"
+
+
+
+
+
+
+# afficher retour HTTP
+#!/usr/bin/env bash
+
+fichier=$1
+number=1
+
+if [ $# -ne 1 ]
+then
+	echo "Aucun fichier saisi, veuillez saisir un chemin";
+	exit;
+fi
+
+while read -r line;
+do
+	http=$(curl -I ${line})
+	echo -e "$number :\t $http"
+	number=$(expr $number + 1)
+done < "$fichier"
