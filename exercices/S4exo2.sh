12e379356067684d8c0c5224e0f8411644096dc4
Zina-Charikh <zineb.charikh@gmail.com>
2023-11-06 15:37:00 +0100

    changement structure

--- exercices/S4exo2.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-# créer un script pour établir le classement des lieux les plus cités.
-cat ./*/*/* | grep Location | cut -f 3 | sort | uniq -c | sort -nr | head
--- exercices/premierscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#écrire un script qui donne le nombre de Location par année
-resultat2016=$(cat ./2016/*/* | grep -c Location)
-resultat2017=$(cat ./2017/*/* | grep -c Location)
-resultat2018=$(cat ./2018/*/* | grep -c Location)
-echo -e "le nombre de locations pour l'années 2016 : $resultat2016"
-echo -e "le nombre de locations pour l'années 2017 : $resultat2017"
-echo -e "le nombre de locations pour l'années 2018 : $resultat2018"
